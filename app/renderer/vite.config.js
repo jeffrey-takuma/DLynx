f799db18844992632e29d850bf0cf99a99efaa51
大川拓馬 <jeftnychysyd@gmail.com>
2026-03-16 23:33:32 +0900

    fix: Electronとrendererの開発起動問題を修正

--- app/renderer/vite.config.js
+++ app/renderer/vite.config.js
@@ -1,6 +1,8 @@
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  plugins: [react()],
   base: "./",
   server: {
     host: "127.0.0.1",
